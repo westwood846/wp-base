ff65208856fac5b90d8ae114b988c1c85aa01396
turbopope <PapstDonB@Googlemail.com>
2016-03-23 13:48:57 +0100

    Set apache DocumentRoot correctly

--- provision.sh
+++ provision.sh
@@ -10,3 +10,6 @@ apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
 echo "create database texwp" | mysql -u root -proot
 
 apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+
+sed -i '12s@.*@\tDocumentRoot /var/www/public@' /etc/apache2/sites-available/000-default.conf
+service apache2 restart
