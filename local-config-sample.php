4448b08a3544f90212c19dad91aa4fc6793a218d
turbopope <PapstDonB@Googlemail.com>
2016-03-23 13:35:27 +0100

    Set up wp configs

---
+++ local-config-sample.php
@@ -0,0 +1,18 @@
+<?php
+define('DB_NAME', 'texwp');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', 'localhost');
+
+ini_set('display_errors', E_ALL );
+define('WP_DEBUG_DISPLAY', true );
+define('WP_DEBUG', true );
+
+define('AUTH_KEY',         '');
+define('SECURE_AUTH_KEY',  '');
+define('LOGGED_IN_KEY',    '');
+define('NONCE_KEY',        '');
+define('AUTH_SALT',        '');
+define('SECURE_AUTH_SALT', '');
+define('LOGGED_IN_SALT',   '');
+define('NONCE_SALT',       '');
---
+++ public/wp-config.php
@@ -0,0 +1,30 @@
+<?php
+ini_set('display_errors', 0);
+
+if (file_exists(dirname(__FILE__) . '/../production-config.php')) {
+  define('WP_LOCAL_DEV', false);
+  include(dirname(__FILE__) . '/../production-config.php');
+} else {
+  define('WP_LOCAL_DEV', true);
+  include(dirname(__FILE__) . '/../local-config.php');
+}
+
+define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content');
+
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+define('WPLANG', '');
+
+define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG', false);
+
+define('AUTOMATIC_UPDATER_DISABLED', false);
+
+$table_prefix  = 'wp_';
+
+if (!defined('ABSPATH')) {
+  define('ABSPATH', dirname(__FILE__) . '/wp/');
+}
+require_once(ABSPATH . 'wp-settings.php');
